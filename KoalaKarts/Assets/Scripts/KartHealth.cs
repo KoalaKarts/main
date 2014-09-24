3dca50f518ce455cd40cbcbe69f460bf86e6c791
felrya <felrya92@live.com>
2014-09-24 09:57:37 -0500

    Updated Health System Script

--- KoalaKarts/Assets/Scripts/KartHealth.cs
+++ KoalaKarts/Assets/Scripts/KartHealth.cs
@@ -3,8 +3,22 @@ using System.Collections;
 
 public class KartHealth : MonoBehaviour
 {
-    private int currentLeaves = 3;
+    private int lives = 3;
+    private int hits = 0;
+    private int currentPoints = 0;
+    private int currentLeaves = 0;
     private int bankedLeaves = 0;
+
+    #region Getters
+
+    /// <summary>
+    /// Getter for current lives.
+    /// </summary>
+    /// <returns> Lives </returns>
+    public int GetLives()
+    {
+        return lives;
+    }
 
     /// <summary>
     /// Getter for kart's current leaves.
@@ -23,6 +37,63 @@ public class KartHealth : MonoBehaviour
     {
         return bankedLeaves;
     }
+
+    /// <summary>
+    /// Getter for kart's current points.
+    /// </summary>
+    /// <returns> Current points </returns>
+    public int GetCurrentPoints()
+    {
+        return currentPoints;
+    }
+
+    #endregion
+
+    #region Lives & Hits
+
+    /// <summary>
+    /// Add life to kart.
+    /// </summary>
+    public void AddLife()
+    {
+        lives++;
+    }
+
+    /// <summary>
+    /// Subtract life from kart. If out of
+    /// lives, call OnDeath().
+    /// </summary>
+    public void SubtractLife()
+    {
+        lives--;
+        if (lives == 0)
+            OnDeath();
+    }
+
+    /// <summary>
+    /// Hit kart.  Subtract a hit and
+    /// if hits == 0, subtract a life.
+    /// </summary>
+    public void Hit()
+    {
+        hits--;
+        if (hits == 0)
+        {
+            SubtractLife();
+        }
+    }
+
+    /// <summary>
+    /// Handle death of player.
+    /// </summary>
+    void OnDeath()
+    {
+        // TODO: Handle death
+    }
+
+    #endregion
+
+    #region Leaves
 
     /// <summary>
     /// Add 1 leaf to the kart's current leaves.
@@ -48,8 +119,6 @@ public class KartHealth : MonoBehaviour
     public void SubtractLeaf()
     {
         currentLeaves--;
-        if (currentLeaves < 0)
-            OnDeath();
     }
 
     /// <summary>
@@ -60,9 +129,49 @@ public class KartHealth : MonoBehaviour
     public void SubtractLeaves(int leaves)
     {
         currentLeaves -= leaves;
-        if (currentLeaves < 0)
-            OnDeath();
     }
+
+    #endregion
+
+    #region Points
+
+    /// <summary>
+    /// Add 1 point to current points.
+    /// </summary>
+    public void AddPoint()
+    {
+        currentPoints++;
+    }
+
+    /// <summary>
+    /// Add specified number of points to
+    /// current points.
+    /// </summary>
+    /// <param name="points"> Points to be added </param>
+    public void AddPoints(int points)
+    {
+        currentPoints += points;
+    }
+
+    /// <summary>
+    /// Subtract 1 point from current points
+    /// </summary>
+    public void SubtractPoint()
+    {
+        currentPoints--;
+    }
+
+    /// <summary>
+    /// Subtract specified number of points
+    /// from current points.
+    /// </summary>
+    /// <param name="points"> Points to subtract </param>
+    public void SubtractPoints(int points)
+    {
+        currentPoints -= points;
+    }
+
+    #endregion
 
     /// <summary>
     /// Bank all current leaves.
@@ -71,13 +180,5 @@ public class KartHealth : MonoBehaviour
     {
         bankedLeaves += currentLeaves;
         currentLeaves = 0;
-    }
-
-    /// <summary>
-    /// Handle death of player.
-    /// </summary>
-    void OnDeath()
-    {
-        // TODO: Handle death
     }
 }
