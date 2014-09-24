ded9a18f9e43f29f492c80c3a6ad848bca96c40d
Reid Baier <baierr5032@my.uwstout.edu>
2014-09-24 14:45:26 -0500

    AI updates
    The cube(AI) will now go in a path. Speed is slow though.

--- KoalaKarts/Assets/Scripts/RacingAI.cs
+++ KoalaKarts/Assets/Scripts/RacingAI.cs
@@ -4,61 +4,44 @@ using System.Collections;
 public class RacingAI : MonoBehaviour {
 	public KartController kc;
 	public KartHealth kh;
+	public ItemSpawner its;
 
-	//where the ai goes to when finding enemy, overall node stuff
-	public GameObject node1;
-	public GameObject node2;
-	public GameObject node3;
-	public GameObject node4;
-	public GameObject node5;
-	public GameObject node6;
-	public GameObject node7;
-	public GameObject node8;
-	public GameObject node9;
-	private Vector3 nodeStart;
-	private Vector3 nodeEnd;
-	public int nodeNum;
-	public GameObject[] nodeArr;
-	//lower the number = go to first
-	public int nodePriority = 1; 
-	//range of nodes which will allow the ai to do certain actions based on what is nearby, need to word better and need to look into more
-	public int nodeRange;
+	/*NODE STUFF*/
+
+	//holds all of the nodes
+	public Transform[] nodeArr;
 	//node the ai will go to, testing with this for now
-	public GameObject nodeTar;
+	private Transform currNode;
+	private int nodeNum;
 
-	//weapon(s) that this character is holding
-	public GameObject weapon;
+	/*WEAPON STUFF*/
+	private bool hasWeapon = false;
+
+	/*COMBAT STUFF*/
+
+	/*AI ATTRIBUTES*/
+	private Vector3 dir;
+	public float speed = 0.5f;
+	//public float speed = kc.GetComponent(speed);
+	public float minDistance = 2.0f;
+	//public int hp = kh.GetComponent (leaves);
+
+
 	//other players
 	public GameObject enemy;
 	//targetted enemy
 	public Transform target;
 
-	private Vector3 dir;
-	public float speed = 0.5f;
-	//public float speed = kc.GetComponent(speed);
-	public float maxDistance = 1.0f;
-	//public int hp = kh.GetComponent (leaves);
-	
 	private bool enemyFound = false;
-	private bool hasWeapon = false;
 	private bool enemyInRange = false;
-	private int randNum;
-
-	void Awake(){
-	}
-
-	// Use this for initialization
-	void Start () {
-		//puts the nodes into the nodeArr, not sure if needed
-		//nodeArr = GameObject.FindGameObjectsWithTag ("node");
-		//for(int i = 0; i < nodeArr.Length; i++){
-
-		//}
-	}
 	
-	// Update is called once per frame
-	void Update () {
+	void Start () {
 
+		currNode = nodeArr [0];
+		nodeNum = 0;
+	}
+
+	void Update () {
 		//searches for a nearby weapon
 		if(!enemyFound && !hasWeapon){
 			//findWeapon();
@@ -71,18 +54,22 @@ public class RacingAI : MonoBehaviour {
 
 		if(!enemyFound && !hasWeapon){// keep hasWeapon false for now, weapons aren't in yet
 			findEnemy ();
+			if(Vector3.Distance (currNode.transform.position, transform.position) < minDistance){
+				nodeNum++;
+				if(nodeNum > nodeArr.Length - 1){
+					nodeNum = 0;
+				}
+				currNode = nodeArr[nodeNum];
+			}
 		}
 	}
 
 	//moves towards node
 	void findEnemy(){
-		nodeTar = node1;
-		transform.position = Vector3.MoveTowards (transform.position, nodeTar.transform.position, speed * Time.deltaTime);	
-		if(transform.position.x == nodeTar.transform.position.x){
-			nodeTar = node2;
-			//Destroy (node1); //ai will still move towards node1 even if node1 is destroyed
-			transform.position = Vector3.MoveTowards (transform.position, nodeTar.transform.position, speed * Time.deltaTime);	
-		}
+		dir = currNode.transform.position - transform.position;
+		Vector3 mov = dir.normalized * speed * Time.deltaTime;
+		transform.position += mov;
+		transform.rotation = Quaternion.Slerp (transform.rotation, Quaternion.LookRotation (dir), 4 * Time.deltaTime);
 	}
 
 	void findWeapon(){
@@ -101,9 +88,24 @@ public class RacingAI : MonoBehaviour {
 
 	}
 
-	/*MANDATORY AI:
+	void OnTriggerEnter(Collider col){
+		if(col.gameObject.name == "ItemSpawner"){
+			Destroy(col.gameObject);
+			hasWeapon = true;
+		}
+	}
+
+	void aiVision(){
+	
+	}
+
+	void foundEnemy(){
+
+	}
+
+	/*MANDATORY AI NEEDED:
 	 * chase- will chase an enemy in the shortest path possible
-	 * find enemy-will go in a predetermined path to different areas of each level trying to find enemies
+	 * find enemy:need to work on turning and stuff
 	 * run away-drive away as fast as possible if low on health
 	 * use weapon- just uses the weapon
 	 * find weapon-searches around for a weapon
