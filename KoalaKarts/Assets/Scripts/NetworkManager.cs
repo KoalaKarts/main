1be719bd7c73aac39c5fe78554c50ef246ac8424
connor-binder <binderc1775@my.uwstout.edu>
2014-09-29 09:30:27 -0500

    Server Initialization
    NetworkManager now initializes a server. Doesn't do much else, but it's
    a start.
    
    I'm following this tutorial:
    http://www.paladinstudios.com/2013/07/10/how-to-create-an-online-multiplayer-game-with-unity/

--- KoalaKarts/Assets/Scripts/NetworkManager.cs
+++ KoalaKarts/Assets/Scripts/NetworkManager.cs
@@ -2,7 +2,31 @@
 using System.Collections;
 
 public class NetworkManager : MonoBehaviour {
+	
+	private const string typeName = "KoalaKartsTheOneAndOnly";
+	private const string gameName = "KoalaRoom";
 
+	private void StartServer()
+	{
+		Network.InitializeServer(6, 25000, !Network.HavePublicAddress());
+		MasterServer.RegisterHost(typeName, gameName);
+		MasterServer.ipAddress = "127.0.0.1";
+	}
+	
+	void OnServerInitialized()
+	{
+		Debug.Log("Server Initialized");
+	}
+
+	void OnGUI()
+	{
+		if(!Network.isClient && !Network.isServer)
+		{
+			if(GUI.Button(new Rect(100, 100, 250, 100), "Start Server"))
+				StartServer();
+		}
+	}
+	
 	// Use this for initialization
 	void Start () {
 	
