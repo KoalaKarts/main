3944e904a862cef90586843ff2e957e9a0054be3
felrya <felrya92@live.com>
2014-10-07 23:24:46 -0500

    Caltrops can be used

---
+++ KoalaKarts/Assets/Scripts/ItemScripts/Caltrops.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Caltrops : MonoBehaviour
+{
+    public float lifetime = 10;
+    private float timer;
+
+    // Use this for initialization
+    void Start()
+    {
+        timer = lifetime;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (timer > 0)
+        {
+            timer -= Time.deltaTime;
+        }
+        else
+        {
+            Destroy(gameObject);
+            timer = lifetime;
+        }
+    }
+}
